9ce291bfd9bccaf8c078ccd553dec77f1d4b10fa
Luky005 <lukynemec05@gmail.com>
2026-04-13 14:19:18 +0200

    Features: prevent first attack, ignore mace cooldown

--- src/main/java/org/simpleautoattack/SimpleAutoAttack/AutoAttack.java
+++ src/main/java/org/simpleautoattack/SimpleAutoAttack/AutoAttack.java
@@ -10,6 +10,7 @@ import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
@@ -86,8 +87,18 @@ public class AutoAttack implements ClientModInitializer {
     }
 
     private void AutoMeleeTick(MinecraftClient mc) {
-        if (!mc.options.attackKey.isPressed() || mc.player == null || mc.world == null || mc.interactionManager == null
-                || !(mc.player.getAttackCooldownProgress(0) >= 1)) {
+        if (!mc.options.attackKey.isPressed()
+                || mc.player == null
+                || mc.world == null
+                || mc.interactionManager == null) {
+            return;
+        }
+
+        ItemStack mainHandItem = mc.player.getMainHandStack();
+
+        // Mace Ignore cooldown
+        boolean ignoreCooldown = getConfig().ignoreMaceCooldown && mainHandItem.getItem().toString().toLowerCase().contains("mace");
+        if (!ignoreCooldown && mc.player.getAttackCooldownProgress(0) < 1) {
             return;
         }
 
--- src/main/java/org/simpleautoattack/SimpleAutoAttack/mixin/AutoAttackClientMixin.java
+++ src/main/java/org/simpleautoattack/SimpleAutoAttack/mixin/AutoAttackClientMixin.java
@@ -9,11 +9,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.registry.tag.ItemTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.simpleautoattack.SimpleAutoAttack.config.AutoAttackConfig;
 import me.shedaniel.autoconfig.AutoConfig;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
@@ -26,34 +28,52 @@ public abstract class AutoAttackClientMixin {
     // Prevents breaking blocks when holding a weapon
     @Inject(method = "handleBlockBreaking(Z)V", at = @At("HEAD"), cancellable = true)
     public void onHandleBlockBreaking(boolean isBreakPressed, CallbackInfo info) {
-        // Get config instance
         AutoAttackConfig config = AutoConfig.getConfigHolder(AutoAttackConfig.class).getConfig();
-        
+
         // Only prevent block breaking if the config option is enabled
         if (config.preventBlockBreaking.enabled && isBreakPressed && player != null) {
             ItemStack mainHandItem = player.getMainHandStack();
-            String itemName = mainHandItem.getItem().toString().toLowerCase();
 
-            boolean shouldPrevent = false;
-            if (mainHandItem.isIn(ItemTags.SWORDS) && config.preventBlockBreaking.sword) {
-                shouldPrevent = true;
-            } else if (itemName.contains("trident") && config.preventBlockBreaking.trident) {
-                shouldPrevent = true;
-            } else if (itemName.contains("mace") && config.preventBlockBreaking.mace) {
-                shouldPrevent = true;
-            } else if (mainHandItem.isIn(ItemTags.AXES) && config.preventBlockBreaking.axe) {
-                shouldPrevent = true;
-            } else if (mainHandItem.isIn(ItemTags.PICKAXES) && config.preventBlockBreaking.pickaxe) {
-                shouldPrevent = true;
-            } else if (mainHandItem.isIn(ItemTags.SHOVELS) && config.preventBlockBreaking.shovel) {
-                shouldPrevent = true;
-            } else if (mainHandItem.isIn(ItemTags.HOES) && config.preventBlockBreaking.hoe) {
-                shouldPrevent = true;
-            }
-
-            if (shouldPrevent) {
+            if (shouldPrevent(mainHandItem, config)) {
                 interactionManager.cancelBlockBreaking();
                 info.cancel();
+            }
+        }
+    }
+
+    @Unique
+    private boolean shouldPrevent(ItemStack stack, AutoAttackConfig config) {
+        String itemName = stack.getItem().toString().toLowerCase();
+
+        if (stack.isIn(ItemTags.SWORDS) && config.preventBlockBreaking.sword) {
+            return true;
+        } else if (itemName.contains("trident") && config.preventBlockBreaking.trident) {
+            return true;
+        } else if (itemName.contains("mace") && config.preventBlockBreaking.mace) {
+            return true;
+        } else if (stack.isIn(ItemTags.AXES) && config.preventBlockBreaking.axe) {
+            return true;
+        } else if (stack.isIn(ItemTags.PICKAXES) && config.preventBlockBreaking.pickaxe) {
+            return true;
+        } else if (stack.isIn(ItemTags.SHOVELS) && config.preventBlockBreaking.shovel) {
+            return true;
+        } else if (stack.isIn(ItemTags.HOES) && config.preventBlockBreaking.hoe) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Prevent first attack
+    @Inject(method = "doAttack", at = @At("HEAD"), cancellable = true)
+    public void doAttack(CallbackInfoReturnable<Boolean> cir) {
+        AutoAttackConfig config = AutoConfig.getConfigHolder(AutoAttackConfig.class).getConfig();
+
+        if (config.enabled && config.preventFirstAttack && player != null) {
+            ItemStack mainHandItem = player.getMainHandStack();
+
+            if (shouldPrevent(mainHandItem, config)) {
+                cir.setReturnValue(false);
             }
         }
     }
